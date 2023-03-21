12ed7b25b1967e5f2879ae4ed78c40d051f0d05f
pszenher <pszenher@stevens.edu>
2023-03-21 16:22:24 -0400

    Disable nezzar home services sound module (incomplete impl.).

--- nezzar/home/services/sound.scm
+++ nezzar/home/services/sound.scm
@@ -1,21 +1,21 @@
-(define-module (nezzar home services sound)
-  #:use-module (gnu home services)
-  #:use-module (gnu home services shepherd)
-  #:use-module (nezzar home services sound)
+;; (define-module (nezzar home services sound)
+;;   #:use-module (gnu home services)
+;;   #:use-module (gnu home services shepherd)
+;;   #:use-module (nezzar home services sound)
 
-  #:export ())
+;;   #:export ())
 
-(define pipewire-activation)
+;; (define pipewire-activation)
 
-(define home-pipewire-service-type
-  (service-type
-   (name 'home-pipewire)
-   (extensions
-    (list (service-extension home-files-service-type
-			     pipewire-configuration-files)
-	  (service-extension home-activation-service-type
-			     pipewire-activation)
-	  (service-extension home-shepherd-service-type
-			     pipewire-shepherd-service)))
-    (description "Configure PipeWire sound support.")
-    (default-value (home-pipewire-configuration))))
+;; (define home-pipewire-service-type
+;;   (service-type
+;;    (name 'home-pipewire)
+;;    (extensions
+;;     (list (service-extension home-files-service-type
+;; 			     pipewire-configuration-files)
+;; 	  (service-extension home-activation-service-type
+;; 			     pipewire-activation)
+;; 	  (service-extension home-shepherd-service-type
+;; 			     pipewire-shepherd-service)))
+;;     (description "Configure PipeWire sound support.")
+;;     (default-value (home-pipewire-configuration))))
