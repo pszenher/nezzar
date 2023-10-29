439a885c9c69d42fec4dc34099a75d5269f0cdfd
Paul Szenher <pszenher@stevens.edu>
2023-10-29 14:27:06 -0400

    Cleanup tpm2-* package inputs, implement tentative tpm-abrmd service

--- nezzar/packages/hardware.scm
+++ nezzar/packages/hardware.scm
@@ -101,15 +101,15 @@ LDFLAGS and parse the output to build extensions with setup.py.")
        (sha256
 	(base32
 	 "0lzm8lm3spjjxrni6pr9vjxv4ggfi1ss4xpil6x4k07hwawdhr13"))))
+    (inputs
+     (list openssl
+	   gmp))
     (native-inputs
-     (list
-      autoconf
-      automake
-      libtool
-      pkg-config
-      perl				; pod2man
-      openssl
-      gmp))
+     (list autoconf
+	   automake
+	   libtool
+	   perl				; pod2man
+	   pkg-config))
     (build-system gnu-build-system)
     (arguments
      '(#:configure-flags '("--with-openssl" "--with-tpm2")
@@ -142,31 +142,30 @@ minimum of necessary APIs are made publicly available.")
        (sha256
 	(base32
 	 "09cnxh5pj71583vlfhp96b5ic4wbd8bwjsg5wgq50c35n55pk920"))))
+    (inputs
+     (list openssl
+	   libtpms
+	   iproute
+	   libseccomp
+
+	   python
+	   
+	   libtasn1
+	   expect
+	   socat
+	   python-twisted
+	   fuse
+	   glib
+	   gnutls
+	   json-glib
+
+	   tpm2-tss-4.0))
     (native-inputs
-     (list
-      autoconf
-      automake
-      libtool
-      pkg-config
-      openssl
-
-      libtpms
-
-      iproute
-      python
-      libseccomp
-      perl 				; pod2man
-
-      libtasn1
-      expect
-      socat
-      python-twisted
-      fuse
-      glib
-      gnutls
-      json-glib
-
-      tpm2-tss-4.0))
+     (list autoconf
+	   automake
+	   libtool
+	   perl 				; pod2man
+	   pkg-config))
     (build-system gnu-build-system)
     (arguments
      '(#:tests? #f
@@ -196,6 +195,9 @@ creation of multiple native /dev/vtpm* devices.")
        (sha256
 	(base32
 	 "0v7iz40f007c3l7aqqy6wj4g4p0pg8dxqm14022sgd8jrc4dqjcp"))))
+    (inputs
+     (list tpm2-tss-4.0
+	   dbus))
     (native-inputs
      (list
       ;; Required dependencies
@@ -206,8 +208,6 @@ creation of multiple native /dev/vtpm* devices.")
       glib
       `(,glib "bin")
       pkg-config
-      tpm2-tss-4.0
-      dbus
       
       ;; Test dependencies
       cmocka))
@@ -238,8 +238,10 @@ implemented using Glib and the GObject system.")
        (sha256
 	(base32 "15y8jlpml37npilpmja84rzqz307maz70f9c7ppsxhgxaqa4nnsv"))
        (patches
-	'(;; Apply patch from 2.2.0-rc0 to fix test w/ newer versions of python-cryptography
-	  "patches/0001-test-add-check-for-renamed-cryptography-types.patch"))))
+	(list
+	 ;; Apply patch from 2.2.0-rc0 to fix test w/ newer versions of python-cryptography
+	 (local-file
+	  "patches/0001-test-add-check-for-renamed-cryptography-types.patch")))))
     (build-system pyproject-build-system)
     (arguments
      `(#:phases
@@ -248,40 +250,37 @@ implemented using Glib and the GObject system.")
               (lambda _
 		(substitute* "test/TSS2_BaseTest.py"
 		  (("libtss2-tcti-swtpm.so") #$(file-append tpm2-tss-4.0 "/lib/libtss2-tcti-swtpm.so"))))))))
-    (propagated-inputs (list python-asn1crypto
-			     python-cffi
-			     python-cryptography
-                             python-packaging python-pyyaml))
-    (native-inputs (list
-		    ;; added
-		    pkg-config
-		    python-pkgconfig-1.5
+    (inputs
+     (list tpm2-tss-4.0))
+    (propagated-inputs
+     (list python-asn1crypto
+	   python-cffi
+	   python-cryptography
+           python-packaging
+	   python-pyyaml))
+    (native-inputs
+     (list pkg-config
+	   python-pkgconfig-1.5
+	   python-black
+	   python-codecov
+	   python-coverage
+	   python-docutils
+	   python-myst-parser
+	   python-pytest
+	   python-pytest-cov
+	   python-pytest-xdist
+	   python-setuptools-scm
+	   python-sphinx
+	   python-sphinx-rtd-theme
+	   python-twine
 
-		    python-black
-                    ;; python-build
-                    python-codecov
-                    python-coverage
-                    python-docutils
-                    ;; python-installer
-                    python-myst-parser
-                    python-pytest
-                    python-pytest-cov
-                    python-pytest-xdist
-                    python-setuptools-scm
-                    python-sphinx
-                    python-sphinx-rtd-theme
-                    python-twine
-
-		    tpm2-tss-4.0
-
-		    ;; Test dependencies
-		    swtpm))
+	   ;; Test dependencies
+	   swtpm))
     (home-page "https://github.com/tpm2-software/tpm2-pytss")
     (synopsis "TPM 2.0 TSS Bindings for Python")
     (description "TPM 2.0 TSS Bindings for Python")
     (license license:bsd-3)))
 
-(use-modules ((gnu packages) #:select (search-auxiliary-file)))
 (define-public tpm2-pkcs11
   (package
     (name "tpm2-pkcs11")
@@ -296,97 +295,46 @@ implemented using Glib and the GObject system.")
        (sha256
 	(base32
 	 "0af0lwg0ljp48c7facrbd7m3c0v43j530dgwwdhd4q28jj0yv0aa"))))
+    (inputs
+     (list
+      libyaml
+      ;; openssl-1.1   ;FIXME:  tpm2-tss must be >= 3.2 to use openssl-3.0
+      openssl
+      sqlite
+      tpm2-abrmd
+      tpm2-tss-4.0))
     (native-inputs
      (list
       autoconf
       autoconf-archive
       automake
       libtool
-      libyaml
-      ;; openssl-1.1   ;FIXME:  tpm2-tss must be >= 3.2 to use openssl-3.0
-      openssl
       pkg-config
-      sqlite
-      tpm2-abrmd
-      tpm2-tss-4.0
 
-      ;; Optional dependencies for tpm2_ptool
+      ;; Test dependencies
+      cmocka
+      swtpm
+
+      ;;  for test phase w/ tpm2_ptool script
       tpm2-tools
-
-      python-wrapper
+      python
       python-bcrypt
       python-cryptography
       python-pyyaml
       python-pyasn1
       python-pyasn1-modules
-      python-tpm2-pytss
-
-      ;; Test dependencies
-      cmocka
-      swtpm))
+      python-tpm2-pytss))
     (build-system gnu-build-system)
-    (outputs '("out" "python"))
     (arguments
      (list
-      #:imported-modules %python-build-system-modules
-      #:modules '((srfi srfi-26)
-		  (guix build utils)
-		  (guix build gnu-build-system)
-		  ((guix build python-build-system) #:prefix python:))
       #:phases
-      (with-imported-modules '((guix build python-build-system))
-	#~(let ((python-phase (lambda (phase)
-				(let ((phase-func (assq-ref python:%standard-phases phase)))
-				  (cut phase-func #:use-setuptools? #t <...> )))))
-	    (modify-phases %standard-phases
-	      (add-before 'bootstrap 'set-version-string
-		(lambda* (#:key version #:allow-other-keys)
-		  (substitute* "bootstrap"
-		    (("git describe --tags --always --dirty") #$(format #f "echo ~a" (package-version this-package))))))
-	      (add-after
-		  'unpack
-		  'ensure-no-mtimes-pre-1980
-		(python-phase 'ensure-no-mtimes-pre-1980))
-	      (add-after
-		  'ensure-no-mtimes-pre-1980
-		  'enable-bytecode-determinism
-		(python-phase 'enable-bytecode-determinism))
-	      (add-after
-		  'enable-bytecode-determinism
-		  'ensure-no-cythonized-files
-		(python-phase 'ensure-no-cythonized-files))
-	      
-	      (add-after
-		  'build
-		  'python:build
-		(python-phase 'build))
-	      (add-after
-		  'python:build
-		  'python:install
-		(python-phase 'install))
-
-	      (add-after
-		  'python:install
-		  'python:add-install-to-pythonpath
-		(python-phase 'add-install-to-pythonpath))
-	      (add-after
-		  'python:add-install-to-pythonpath
-		  'python:add-install-to-path
-		(python-phase 'add-install-to-path))
-	      (add-after
-		  'python:add-install-to-path
-		  'python:wrap
-		(python-phase 'wrap))
-	      (add-after
-		  'python:wrap
-		  'python:check
-		(python-phase 'check))
-
-	      ;; Directory excursion for python build/install steps
-	      (add-before 'python:build 'python:chdir-down
-		(lambda _ (chdir "tools")))
-	      (add-after 'python:check 'python:chdir-up
-		(lambda _ (chdir ".."))))))))
+      #~(modify-phases %standard-phases
+	 (add-before 'bootstrap 'set-version-string
+	   (lambda* (#:key version #:allow-other-keys)
+	     (substitute* "bootstrap"
+	       (("git describe --tags --always --dirty")
+		(string-append
+		 "echo " #$(package-version this-package)))))))))
     (home-page "https://tpm2-software.github.io/")
     (synopsis "A PKCS#11 interface for TPM2 hardware")
     (description
@@ -396,4 +344,155 @@ hardware security modules (HSM), smart cards, etc.  In this project we
 intend to use a TPM2 device as the cryptographic token.")
     (license license:bsd-2)))
 
-tpm2-pkcs11
+(define-public python-tpm2-pkcs11
+  (package
+    (inherit tpm2-pkcs11)
+    (name "python-tpm2-pkcs11")
+    (build-system python-build-system)
+    (inputs
+     (list
+      tpm2-pkcs11
+      tpm2-tools))
+    (propagated-inputs
+     (list python-bcrypt
+	   python-cryptography
+	   python-pyyaml
+	   python-pyasn1
+	   python-pyasn1-modules
+	   python-tpm2-pytss))
+    (arguments
+     (list
+      #:phases
+      #~(modify-phases %standard-phases
+	  (add-after 'unpack 'chdir-tools (lambda _ (chdir "tools")))
+	  (add-after 'chdir-tools 'fully-qualify-tpm2-tool-paths
+	    (lambda* (#:key inputs #:allow-other-keys)
+	      (let ((tpm2-tools (assoc-ref inputs "tpm2-tools"))
+		    (quote-wrap (lambda (str) (string-append "'" str "'"))))
+		(substitute* "tpm2_pkcs11/tpm2.py"
+		  (("'(tpm2_[A-Za-z0-9]+)'" _ command_name)
+		   (quote-wrap
+		    (string-append
+		     tpm2-tools "/bin/" command_name))))))))))
+    (synopsis "A PKCS#11 Python interface for TPM2 hardware")))
+
+;;;
+;;; TPM 2.0 Access Broker and Resource Manager Daemon
+;;;
+
+(use-modules (gnu services utils))
+
+(define-configuration tpm2-abrmd-configuration
+  (package
+   (package tpm2-abrmd)
+   "@code{tpm2-abrmd} Guix Package")
+  (tcti
+   (tcti-string "libtss2-tcti-device.so.0:/dev/tpm0")
+   "A string that describes the @acronym{TCTI, @acronym{TPM} Command
+Transition Interface} and how to configure it for communication with
+the next component down the @acronym{TSS2, TPM2 Software Stack}
+stack. This string is formatted as @code{@var{tcti-name}:@var{tcti-conf}} where:
+
+@table @var
+
+@item tcti-name
+The name of the @acronym{TCTI} library shared object file. Libraries
+are found using the same algorithm as @cite{dlopen (3)}. If the
+@acronym{TCTI} library file name follows the naming convention:
+@code{libtss2-tcti-@var{<name>}.so.0} where @var{<name>} is the name
+for the @acronym{TCTI}, the value of @var{<name>} may be supplied in
+place of the full library file name.
+
+@item tcti-conf
+The configuration string passed to the @acronym{TCTI} library upon
+initialization.
+
+@end table
+
+If this option is omitted (or a @code{NULL} string provided) then a
+default @acronym{TCTI} is used in it's default configuration. If the
+string does not contain a colon then it will be interpreted as only
+the @var{tcti-name}. To provide only the configuration string (using
+the default @acronym{TCTI}) then the first character in the string
+passed to this option must be a colon followed by the configuration
+string.")
+  (allow-root
+   (boolean #f)
+   "Allow daemon to run as root. If this option is not provided the daemon
+will refuse to run as the @code{root} user. Use of this option is @strong{not}
+recommended.")
+  (max-connections
+   (number 27)
+   "Set an upper bound on the number of concurrent client connections
+allowed.  Once this number of client connections is reached new
+connections will be rejected with an error.  If the option is not
+specified the default is @code{27}.")
+  (flush-all
+   (boolean #f)
+   "Flush all objects and sessions when daemon is started.")
+  (logger
+   (logging-target "stdout")
+   "Direct logging output to named logging target. Supported targets are
+@code{stdout} and @code{syslog}. If the logger option is not specified
+the default is @code{stdout}.")
+  (max-sessions
+   (number 4)
+   "Set and upper bound on the number of sessions that each client
+connection is allowed to create (loaded or active) at any one time.
+If the option is not specified the default is @code{4}.")
+  (max-transients
+   (number 27)
+   "Set an upper bound on the number of transient objects that each client
+connection allowed to load. Once this number of objects is reached
+attempts to load new transient objects will produce an error. If the
+option is not specified the default is @samp{27}.")
+  (dbus-name
+   (string "com.intel.tss2.Tabrmd")
+   "Claim the given name on @code{dbus}. This option overrides the default
+of @samp{com.intel.tss2.Tabrmd}.")
+  (prng-seed-file
+   (maybe-string #f)
+   "Read seed for pseudo-random number generator from the provided file.")
+  (session
+   (boolean #f)
+   "Connect daemon to the session dbus. If the option is not specified the
+daemon connects to the system dbus."))
+
+;; (define (serialize-tpm2-abrmd-configuration config)
+;;   (match-record config <tpm2-abrmd-configuration>
+;; 		))
+
+(define (tpm2-abrmd-shepherd-service config)
+  (match-record config <tpm2-abrmd-configuration> (package)
+    (shepherd-service
+     (documentation "TPM 2.0 Access Broker and Resource Manager Daemon")
+     (provision '(tpm2-abrmd))
+     (requirement '(syslogd))
+     (start #~(make-forkexec-constructor
+	       (cons #$(file-append package "/sbin/tpm2-abrmd")
+		     (config->cmdline-args config))
+	       #:user "tss"
+	       #:group "tss"
+	       #:log-file "/var/log/tpm2-abrmd.log"))
+     (stop #~(make-kill-destructor)))))
+
+(define (tpm2-abrmd-environment _)
+  '(("TPM2_PKCS11_TCTI" . "tabrmd:")))
+
+(define tpm2-abrmd-service-type
+  (service-type
+   (name 'tpm2-abrmd)
+   (description
+    "Run @command{tpm2-abrmd}, the TPM 2.0 Access Broker and Resource Manager Daemon")
+   (extensions
+    (list
+     (service-extension session-environment-service-type
+			pipewire-environment)
+     (service-extension shepherd-root-service-type
+			(compose list tpm2-abrmd-shepherd-service))))))
+
+;; (list
+;;  tpm2-pkcs11
+;;  python-tpm2-pkcs11)
+
+tpm2-abrmd
