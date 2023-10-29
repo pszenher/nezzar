70b5f4e76607ce00ef2c13af7021423966f28324
Paul Szenher <pszenher@stevens.edu>
2023-10-29 14:59:10 -0400

    Remove nezzar fuzzel package definition, added to upstream

--- nezzar/packages/wm.scm
+++ nezzar/packages/wm.scm
@@ -12,36 +12,3 @@
   #:use-module (gnu packages gtk)
   #:use-module (gnu packages gnome)
   #:use-module (gnu packages image))
-
-(define-public fuzzel
-  (package
-    (name "fuzzel")
-    (version "1.7.0")
-    (source (origin
-	      (method git-fetch)
-	      (uri (git-reference
-		    (url "https://codeberg.org/dnkl/fuzzel.git")
-		    (commit version)))
-	      (file-name (git-file-name name version))
-	      (sha256
-	       (base32
-		"1261gwxiky37pvzmmbrpml1psa22kkglb141ybj1fbnwg6j7jvlf"))
-	      (modules '((guix build utils)))
-	      ;; Delete 3rd-party directory containing nanosvg headers
-	      (snippet '(delete-file-recursively "3rd-party"))))
-    (build-system meson-build-system)
-    (arguments
-     '(#:build-type "release"
-       #:configure-flags '("-Denable-cairo=enabled"
-			   "-Dpng-backend=libpng"
-			   "-Dsvg-backend=librsvg")))
-    (inputs
-     (list pixman wayland libxkbcommon fcft cairo libpng librsvg))
-    (native-inputs
-     (list pkg-config wayland-protocols scdoc tllist))
-    (home-page "https://codeberg.org/dnkl/fuzzel")
-    (synopsis "Application launcher for wlroots based Wayland compositors")
-    (description
-     "Fuzzel s a Wayland-native applcation launcher, similar to rofi's 'drun' mode.")
-    (license license:expat)))
-
